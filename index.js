df38061127d296c66e45bbfc027c0cf4a49ecbdf
Na Yoon Kang <61221081+nykang17@users.noreply.github.com>
2024-10-16 01:17:15 -0700

    first upload of the code

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = process.env.PORT || 5500
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '/public','/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+});
+
+server.listen(PORT);
